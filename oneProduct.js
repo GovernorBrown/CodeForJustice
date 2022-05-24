2ce9a769d3dfc8c3dee1709a5a965e790516097c
Governor Brown <GovernorBrown@MacBook-Pro-5.local>
2022-05-24 10:06:03 -0400

    small stuff

--- oneProduct.js
+++ oneProduct.js
@@ -4,6 +4,7 @@
     {"id": "2","name":"Shirt 2", "avatar":"./photos/shopping-3.jpeg" , "description": "This is Noble shirt 2."}
   ]
   function loadId(){
+    //KEYS WITH PROPERTIES FOR INDIVIDUAL PRODUCTS//
     let arrayIds = [
       {"id": "0", "name":"Shirt 0", "avatar":"photos/shopping-2.png" , "description": "This is Noble shirt 0."},
       {"id": "1","name":"Shirt 1", "avatar":"photos/shopping-2.png" , "description": "This is Noble shirt 1."},
